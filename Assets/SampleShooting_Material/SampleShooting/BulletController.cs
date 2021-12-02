58ba7e77e705f8696b501c9cb599c82bf1222df5
yutorikohei <yutoriseikatu49@gmail.com>
2021-12-02 12:06:06 +0900

    shooting1
    saisho

---
+++ Assets/SampleShooting_Material/SampleShooting/BulletController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+
+public class BulletController : MonoBehaviour {
+
+	public GameObject explosionPrefab;   //爆発エフェクトのPrefab
+	
+	void Update () {
+		transform.Translate (0, 0.02f, 0);
+
+		if (transform.position.y > 5) {
+			Destroy (gameObject);
+		}
+	}
+	void OnTriggerEnter2D(Collider2D coll) {
+	        // 爆発エフェクトを生成する	
+		Instantiate (explosionPrefab, transform.position, Quaternion.identity);
+		Destroy (coll.gameObject);	
+		Destroy (gameObject);
+	}
+}
