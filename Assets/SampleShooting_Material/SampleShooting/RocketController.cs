9743653421008a515dfc8ce0b96b0e4c3bd40ec8
makilibe <51898594+makilibe@users.noreply.github.com>
2022-01-09 19:56:18 +0900

    Revert "Game script"

--- Assets/SampleShooting_Material/SampleShooting/RocketController.cs
+++ Assets/SampleShooting_Material/SampleShooting/RocketController.cs
@@ -1,53 +1,19 @@
 using UnityEngine;
-using System.Collections.Generic;
 using System.Collections;
 
 public class RocketController : MonoBehaviour {
 
 	public GameObject bulletPrefab;
-    public GameObject explosionPrefab;
 
-    GameObject csvreader;
-
-    public AudioClip Ca;
-    public AudioClip Ia;
-    AudioSource audioSource;
-
-    void Start ()
-    {
-        csvreader = GameObject.Find("csvreader");
-        audioSource = GetComponent<AudioSource>();
-    }
-
-    void Update () {
+	void Update () {
 		if (Input.GetKey (KeyCode.LeftArrow)) {
-			transform.Translate (-0.005f, 0, 0);
+			transform.Translate (-0.01f, 0, 0);
 		}
 		if (Input.GetKey (KeyCode.RightArrow)) {
-			transform.Translate ( 0.005f, 0, 0);
+			transform.Translate ( 0.01f, 0, 0);
 		}
 		if (Input.GetKeyDown (KeyCode.Space)) {
-            Instantiate (bulletPrefab, transform.position, Quaternion.identity);
-        }
+			Instantiate (bulletPrefab, transform.position, Quaternion.identity);
+		}
 	}
-    void OnTriggerEnter2D(Collider2D coll)
-    {
-        // 爆発エフェクトを生成する	
-        if (coll.gameObject.name == "Rockprefab(Clone)")
-        {
-            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
-            Destroy(coll.gameObject);
-            Destroy(gameObject);
-            csvreader.GetComponent<csvread>().DelayendMethod();
-        }
-    }
-
-    public void clearAccess()
-    {
-        audioSource.PlayOneShot(Ca);
-    }
-    public void IclearAccess()
-    {
-        audioSource.PlayOneShot(Ia);
-    }
 }
